5751b47cab29f966a0122edf0e4418754c5efd3f
Allen Conlon <software@conlon.dev>
2025-07-17 20:38:14 -0400

    feat: update the components

--- .github/hack/zarf.sh
+++ .github/hack/zarf.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -u
+set -o pipefail
 
 declare -a FLAVOR=("upstream")
 declare -a COMPONENTS=("operator" "cluster")
@@ -10,15 +11,16 @@ for flavor in "${FLAVOR[@]}"; do
   rm -rf .direnv/$flavor
   mkdir -p .direnv/$flavor
   zarf dev find-images --flavor $flavor --skip-cosign . 2>/dev/null > .direnv/$flavor/out.yml
+  echo "::debug::${flavor}"
   for component in "${COMPONENTS[@]}"; do
     echo "::debug::${component}"
     for type in "${COMPONENTS_TYPES[@]}"; do
       echo "::debug::${type}"
-      export yq_search=$(printf '.components[] | select(.name == "%s%s" and .only.flavor == "%s") | path | .[1]' "$component" "$type" "$flavor")
+      export yq_search=$(printf '.components[] | select(.name == "%s%s-images" and .only.flavor == "%s") | path | .[1]' "$component" "$type" "$flavor")
       echo "::debug::${yq_search}"
       export yq_index=$(yq "$yq_search" zarf.yaml)
       echo "::debug::${yq_index}"
-      export yq_images=$(printf '(select(fi == 1) | .components[] | select(.name == "%s") | .images | ... head_comment="") as $img' "$component")
+      export yq_images=$(printf '(select(fi == 1) | .components[] | select(.name == "%s-chart") | .images | ... head_comment="") as $img' "$component")
       echo "::debug::${yq_images}"
       export yq_charts=$(printf '(select(fi == 0) | .x-charts.%s) as $cht' "$component")
       echo "::debug::${yq_charts}"
